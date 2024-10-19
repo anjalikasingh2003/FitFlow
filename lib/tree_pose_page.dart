d76332ab93c1cedad9ca9b66ef38c5f8e002d3f8
shreya_2413 <shreyajain02597@gmail.com>
2024-10-19 19:10:12 +0530

    updating tree_pose_page

--- lib/tree_pose_page.dart
+++ lib/tree_pose_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:io';
 
 class TreePosePage extends StatelessWidget {
   @override
@@ -8,11 +10,36 @@ class TreePosePage extends StatelessWidget {
         title: Text('Tree Pose'),
       ),
       body: Center(
-        child: Text(
-          'Information about Tree Pose will be displayed here.',
-          style: TextStyle(fontSize: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Display the image
+            Image.asset('assets/img_tree_pose.jpg'), // Ensure the path is correct
+
+            SizedBox(height: 20), // Space between image and button
+
+            // Display the Start button
+            ElevatedButton(
+              onPressed: () {
+                _runTreePoseScript();
+              },
+              child: Text('Start'),
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  // Function to run the Tree_pose.py script
+  void _runTreePoseScript() async {
+    // Make sure the path to the script is correct
+    Process.run('python', ['yoga_poses/Tree_pose.py']).then((result) {
+      // Handle the output of the script
+      print(result.stdout);
+      print(result.stderr);
+    }).catchError((e) {
+      print('Error running script: $e');
+    });
   }
 }
